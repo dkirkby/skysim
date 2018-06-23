6ac31704cbc38c68ad780a41cad104f33275aa70
David Kirkby <dkirkby@uci.edu>
2018-06-23 10:27:13 -0700

    Add prepare_data script

---
+++ skysim/scripts/__init__.py
@@ -0,0 +1,2 @@
+"""Scripts used by this package.
+"""
---
+++ skysim/scripts/prepare_data.py
@@ -0,0 +1,129 @@
+"""Prepare the data files that accompany this package.
+
+This script does not normally need to be run since the package already includes
+the prepared data.  It is provided to document how the data was prepared.
+
+Running this script requires that the skycalc_cli package is installed.
+
+To run this script, use::
+
+    python -m skysim.scripts.prepare_data
+"""
+import os.path
+import tempfile
+
+import astropy.table
+import astropy.utils.data
+import astropy.constants
+import astropy.units as u
+
+import skysim.airglow
+
+
+def prepare_atmosphere(args=None):
+    """Call the ESO SkyCalc CLI and save atmospheric data used by this package.
+
+    Data consists of zenith transmission curves and extinction-corrected
+    airglow emission spectra tabulated on a 0.01nm grid covering 300-1100nm.
+
+    Data are saved to a FITS file data/atmosphere.fits relative to this
+    package's installation directory.
+    """
+    try:
+        import skycalc_cli.skycalc
+    except ImportError as e:
+        print('The skycalc_cli package is not installed.')
+    # Specify how SkyCalc will be called.
+    params = dict(
+        airmass=1.,
+        season=0,            # annual average
+        time=0,              # nightly average
+        vacair='vac',        # vaccuum wavelengths
+        wmin=300.,           # nm
+        wmax=1100.,          # nm
+        wdelta=0.01,         # nm
+        observatory='2640',  # paranal
+        incl_starlight='N',
+        incl_moon='N',
+        incl_zodiacal='N',
+        incl_airglow='Y'
+    )
+    print('Calling the ESO SkyCalc...')
+    sc = skycalc_cli.skycalc.SkyModel()
+    sc.callwith(params)
+    with tempfile.NamedTemporaryFile() as f:
+        f.write(sc.data)
+        f.flush()
+        t = astropy.table.Table.read(f.name)
+        # Build a new table with only the columns we need.
+        tnew = astropy.table.Table()
+        tnew['wavelength'] = astropy.table.Column(
+            t['lam'], description='Vacuum wavelength in nm', unit='nm')
+        tnew['trans_ma'] = astropy.table.Column(
+            t['trans_ma'], description=
+            'Zenith transmission fraction for molecular absorption')
+        tnew['trans_o3'] = astropy.table.Column(
+            t['trans_o3'], description=
+            'Zenith transmission fraction for ozone absorption')
+        # Undo absorption and scattering extinction of airglow.
+        ael = t['flux_ael']
+        arc = t['flux_arc']
+        rs = t['trans_rs']
+        ms = t['trans_ms']
+        ma = t['trans_ma']
+        nonzero = ma > 0
+        arc[nonzero] /= ma[nonzero]
+        ael[nonzero] /= ma[nonzero]
+        fR, fM = skysim.airglow.airglow_scattering(0)
+        scattering = rs ** fR * ms ** fM
+        arc /= scattering
+        ael /= scattering
+        # Convert from flux density per um to per nm and save.
+        tnew['airglow_cont'] = astropy.table.Column(
+            1e3 * arc, description='Unextincted airglow continuum',
+            unit='ph / (arcsec2 m2 s um)')
+        tnew['airglow_line'] = astropy.table.Column(
+            1e3 * arc, description='Unextincted airglow narrow lines',
+            unit='ph / (arcsec2 m2 s um)')
+    # Save the new table.
+    path = astropy.utils.data._find_pkg_data_path('../data/atmosphere.fits')
+    tnew.write(path)
+    print(f'Wrote {len(tnew)} rows to {path}')
+
+
+def prepare_solarspec():
+    """Prepare the solar spectrum used by this package.
+
+    Download the STIS reference solar spectrum and convert to the format used by
+    this package by restricting to optical wavelengths (300-1100nm) and save
+    as a binary FITS table.
+
+    This solar spectrum is documented in `Bohlin, Dickinson, & Calzetti 2001,
+    AJ, 122, 2118 <https://doi.org/10.1086/323137>`_ and tabulates solar flux
+    above the atmosphere.
+    """
+    print('Downloading reference solar spectrum...')
+    t = astropy.table.Table.read(
+        'ftp://ftp.stsci.edu/cdbs/current_calspec/sun_reference_stis_002.fits')
+    lam = t['WAVELENGTH'].data
+    flux = t['FLUX'].data
+    optical = (lam >= 3000) & (lam <= 11000)  # units are Angstrom
+    tnew = astropy.table.Table()
+    tnew['wavelength'] = astropy.table.Column(
+        0.1 * lam[optical], 'Vacuum wavelength', unit='nm')
+    # Convert flux from erg / (s cm2 A) to ph / (s cm2 nm)
+    hc = astropy.constants.h * astropy.constants.c
+    energy_per_photon = (hc / (lam[optical] * u.Angstrom)).to(u.erg).value
+    conv = 10 / energy_per_photon
+    tnew['flux'] = astropy.table.Column(
+        conv * flux[optical], 'Solar flux above the atmosphere',
+        unit='ph / (s cm2 nm)')
+    # Save the new table.
+    path = astropy.utils.data._find_pkg_data_path('../data/solarspec.fits')
+    tnew.write(path)
+    print(f'Wrote {len(tnew)} rows to {path}')
+
+
+if __name__ == '__main__':
+    prepare_atmosphere()
+    prepare_solarspec()
