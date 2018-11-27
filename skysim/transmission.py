e04bb4f8bb04d84d11cbdf261cbd7538ef5d326f
David Kirkby <dkirkby@uci.edu>
2018-11-27 10:20:03 -0800

    Implement optical depth calculations

--- skysim/transmission.py
+++ skysim/transmission.py
@@ -4,6 +4,8 @@ Refer to Section 2 of Noll 2012 for details.
 """
 import numpy as np
 
+import skysim.utils.resample
+
 
 def tau0R(lam, p=744., H=2.64):
     """Calculate zenith optical depth due to Rayleigh scattering.
@@ -61,3 +63,55 @@ def tau0M(lam, lam0=400., k0=0.013, alpha=-1.38):
     """
     lam = 1e-3 * np.asarray(lam)  # convert from nm to um
     return 0.4 * np.log(10) * k0 * np.power(np.maximum(lam, 1e-3 * lam0), alpha)
+
+
+def tau0ma(lam):
+    """Calculate zenith optical depth of molecular absorption.
+    
+    The main absorbers are the molecular oxygen bands (A ~762nm, B ~688nm,
+    gamma ~628nm) and water vapor bands (~720nm, 820nm, 940nm).
+    See Section 2 and Fig.2 of Noll 2012 for details.
+    
+    Narrow asorption features are resampled to the requested wavelength
+    grid using a flux-conserving algorithm.
+    
+    Parameters
+    ----------
+    lam : float or array
+        Wavelength in microns.
+
+    Returns
+    -------
+    float or array
+        Zenith optical depth of molecular absorption.
+    """
+    atm = skysim.utils.data.get('atmosphere')
+    transmission = skysim.utils.resample.resample_density(
+        lam, atm['wavelength'].data, atm['trans_ma'].data)
+    return -np.log(transmission)
+
+
+def tau0o3(lam):
+    """Calculate zenith optical depth of ozone absorption.
+    
+    The main features are the Huggins band in the near-UV and the
+    broad Chappuis bands around 600nm.
+    See Section 2 and Fig.2 of Noll 2012 for details.
+    
+    Narrow asorption features are resampled to the requested wavelength
+    grid using a flux-conserving algorithm.
+    
+    Parameters
+    ----------
+    lam : float or array
+        Wavelength in microns.
+
+    Returns
+    -------
+    float or array
+        Zenith optical depth of molecular absorption.
+    """
+    atm = skysim.utils.data.get('atmosphere')
+    transmission = skysim.utils.resample.resample_density(
+        lam, atm['wavelength'].data, atm['trans_o3'].data)
+    return -np.log(transmission)
