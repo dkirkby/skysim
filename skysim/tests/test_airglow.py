Fix airglow units

skysim/tests/test_airglow.py
@@ -21,6 +21,7 @@ def test_airglow_scattering():
 
 def test_get_airglow():
     lam = np.linspace(300, 1100, 5)
-    #assert np.allclose(
-    #    np.round(get_airglow(lam, 0), 1),
-    #    0.)
+    assert np.allclose(
+        np.round(get_airglow(lam, 0), 3),
+        [[0.035, 0.074, 0.11 , 0.293, 0.216],
+         [0.005, 0.029, 0.194, 0.814, 1.552]])