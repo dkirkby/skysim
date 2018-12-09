c78e50f9b3afad6d8bddb4849ebc14247828d05b
David Kirkby <dkirkby@uci.edu>
2018-12-09 06:49:47 -0800

    prepare_data uses eso.get_skycalc

--- skysim/scripts/prepare_data.py
+++ skysim/scripts/prepare_data.py
@@ -10,7 +10,7 @@ To run this script, use::
     python -m skysim.scripts.prepare_data
 """
 import os.path
-import tempfile
+import argparse
 
 import astropy.table
 import astropy.utils.data
@@ -18,6 +18,7 @@ import astropy.constants
 import astropy.units as u
 
 import skysim.airglow
+import skysim.eso
 
 
 def prepare_atmosphere(overwrite=True):
@@ -29,6 +30,8 @@ def prepare_atmosphere(overwrite=True):
     Data are saved to a FITS file data/atmosphere.fits relative to this
     package's installation directory.
 
+    Uses :func:`skysim.eso.get_skycalc`.
+
     Parameters
     ----------
     overwrite : bool
@@ -37,11 +40,6 @@ def prepare_atmosphere(overwrite=True):
     path = astropy.utils.data._find_pkg_data_path('../data/atmosphere.fits')
     if not overwrite and os.path.exists(path):
         print('Atmosphere file exists and overwrite is False.')
-        return
-    try:
-        import skycalc_cli.skycalc
-    except ImportError as e:
-        print('The skycalc_cli package is not installed.')
         return
     # Specify how SkyCalc will be called.
     params = dict(
@@ -59,42 +57,38 @@ def prepare_atmosphere(overwrite=True):
         incl_airglow='Y'
     )
     print('Calling the ESO SkyCalc...')
-    sc = skycalc_cli.skycalc.SkyModel()
-    sc.callwith(params)
-    with tempfile.NamedTemporaryFile(mode='w+b', buffering=0) as tmpf:
-        tmpf.write(sc.data)
-        t = astropy.table.Table.read(tmpf.name)
-        # Build a new table with only the columns we need.
-        tnew = astropy.table.Table()
-        tnew['wavelength'] = astropy.table.Column(
-            1e3 * t['lam'].data, description='Vacuum wavelength in nm',
-            unit='nm')
-        tnew['trans_ma'] = astropy.table.Column(
-            t['trans_ma'].data,
-            description='Zenith transmission frac for molecular absorption')
-        tnew['trans_o3'] = astropy.table.Column(
-            t['trans_o3'].data,
-            description='Zenith transmission fraction for ozone absorption')
-        # Undo absorption and scattering extinction of airglow.
-        ael = t['flux_ael']
-        arc = t['flux_arc']
-        rs = t['trans_rs']
-        ms = t['trans_ms']
-        ma = t['trans_ma']
-        nonzero = ma > 0
-        arc[nonzero] /= ma[nonzero]
-        ael[nonzero] /= ma[nonzero]
-        fR, fM = skysim.airglow.airglow_scattering(0)
-        scattering = rs ** fR * ms ** fM
-        arc /= scattering
-        ael /= scattering
-        # Convert from flux density per um to per nm and save.
-        tnew['airglow_cont'] = astropy.table.Column(
-            1e-3 * arc, description='Unextincted airglow continuum',
-            unit='ph / (arcsec2 m2 s nm)')
-        tnew['airglow_line'] = astropy.table.Column(
-            1e-3 * ael, description='Unextincted airglow narrow lines',
-            unit='ph / (arcsec2 m2 s nm)')
+    t = skysim.eso.get_skycalc(params)
+    # Build a new table with only the columns we need.
+    tnew = astropy.table.Table()
+    tnew['wavelength'] = astropy.table.Column(
+        1e3 * t['lam'].data, description='Vacuum wavelength in nm',
+        unit='nm')
+    tnew['trans_ma'] = astropy.table.Column(
+        t['trans_ma'].data,
+        description='Zenith transmission frac for molecular absorption')
+    tnew['trans_o3'] = astropy.table.Column(
+        t['trans_o3'].data,
+        description='Zenith transmission fraction for ozone absorption')
+    # Undo absorption and scattering extinction of airglow.
+    ael = t['flux_ael']
+    arc = t['flux_arc']
+    rs = t['trans_rs']
+    ms = t['trans_ms']
+    ma = t['trans_ma']
+    nonzero = ma > 0
+    arc[nonzero] /= ma[nonzero]
+    ael[nonzero] /= ma[nonzero]
+    fR, fM = skysim.airglow.airglow_scattering(0)
+    scattering = rs ** fR * ms ** fM
+    arc /= scattering
+    ael /= scattering
+    # Convert from flux density per um to per nm and save.
+    tnew['airglow_cont'] = astropy.table.Column(
+        1e-3 * arc, description='Unextincted airglow continuum',
+        unit='ph / (arcsec2 m2 s nm)')
+    tnew['airglow_line'] = astropy.table.Column(
+        1e-3 * ael, description='Unextincted airglow narrow lines',
+        unit='ph / (arcsec2 m2 s nm)')
     # Save the new table.
     tnew.write(path, overwrite=overwrite)
     print(f'Wrote {len(tnew)} rows to {path}')
@@ -142,8 +136,15 @@ def prepare_solarspec(overwrite=True):
 
 
 def main():
-    prepare_atmosphere()
-    prepare_solarspec()
+    parser = argparse.ArgumentParser(description='Prepare skysim data.')
+    parser.add_argument(
+        'dataset', nargs='?', choices=['all', 'atmosphere', 'solar',],
+        default='all', help='datasets to prepare')
+    args = parser.parse_args()
+    if args.dataset in ('all', 'atmosphere'):
+        prepare_atmosphere()
+    if args.dataset in ('all', 'solar'):
+        prepare_solarspec()
 
 
 if __name__ == '__main__':
